84ea32b57b4ca4723c0ba29c457cca196b8ed5ff
Andrew Short <andrewjshort@gmail.com>
2015-10-05 16:14:21 +1100

    Remove google functionality.

--- araa.php
+++ araa.php
@@ -1,15 +1,11 @@
 <?php
-/*
-Plugin Name: ARAA
-Plugin URI:  http://wordpress.org/extend/plugins/health-check/
-Description: Custom functionality for the Australian Robotics and Automation Association site
-Author: Andrew Short
-Author URI:  mailto:andrewjshort@gmail.com
+/**
+ * @wordpress-plugin
+ * Plugin Name: ARAA
+ * Description: Custom functionality for the Australian Robotics and Automation Association site
+ * Author: Andrew Short
+ * Author URI: mailto:andrewjshort@gmail.com
  */
-
-require_once __DIR__ . '/admin.php';
-
-// Individual registration.
 
 add_action('wp_enqueue_scripts', function() {
     wp_register_style('araa', plugins_url('/assets/style.css' , __FILE__));
